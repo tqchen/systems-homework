cf2cc90dcaa55652bb27da8cf3e8b69542b26682
tqchen <tianqi.tchen@gmail.com>
2014-10-08 16:28:00 -0700

    finish handler

--- problem1/partb/550server.cpp
+++ problem1/partb/550server.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
+#include <fcntl.h>
 #include <unistd.h>
 #include <sys/poll.h>
 #include <sys/types.h>
@@ -27,15 +28,21 @@ class Server {
       int ret = poll(&fds[0], fds.size(), 3000);
       utils::Check(ret != -1, "poll error");
       if (ret == 0) continue;
+      
       for (size_t i = 0; i < states.size(); ++i) {
-        if (fds[i].revents & fds[i].events) {
-          
+        if (fds[i].revents & fds[i].events) {          
           // handle event happened
           switch (states[i]->type) {
             case State::kGetName: this->HandleGetName(states[i]); break;
+            case State::kLoadData: this->HandleLoadData(states[i]); break;
+            case State::kSendBack: this->HandleSendBack(states[i]); break;
             default: utils::Error("BUG, find unexpected pollfd");
           }
         }
+      }
+      this->CleanClosedStates();
+      if (fds.back().revents & fds.back().events) {
+        this->HandleListen();
       }
     }
   }
@@ -64,7 +71,7 @@ class Server {
     // data blob, storing the content of data
     std::string data_blob;
     // the end of sending buffer
-    size_t send_end;
+    size_t sent_bytes;
   };
   inline void HandleGetName(State *s) {
     size_t len = recv(s->sockfd, recvbuffer, 256, 0);
@@ -75,9 +82,60 @@ class Server {
       // change state here
       s->file_name.resize(n+len-1);
       pipe(s->pipefd);
+      // set pipe to non blocking
+      fcntl(s->pipefd[0], fcntl(s->pipefd[0], F_GETFL) | O_NONBLOCK);
+      // switch to load data
+      s->type = State::kLoadData;
       thread_pool->AddJob(FileLoader, s);
-    }    
+    }
   }
+  inline void HandleLoadData(State *s) {
+    char sig;
+    if (read(s->pipefd[0], &sig, sizeof(sig)) == 0) return;
+    utils::Check(sig == '\0', "BUG");
+    // close the necessary pipe
+    close(s->pipefd[0]);
+    close(s->pipefd[1]);
+    
+    if (s->type == State::kLoadData) {
+      // finish loading
+      s->sent_bytes = 0;
+      s->type = State::kSendBack;    
+    } else {
+      utils::Check(s->type == State::kFailLoad, "type must be loading or fail to load");
+      close(s->sockfd);
+      // fail to load
+      s->type = State::kClosed;
+    }
+  }
+  inline void HandleSendBack(State *s) {
+    size_t &sent_bytes = s->sent_bytes;
+    const std::string &data_blob = s->data_blob;
+    size_t len = send(s->sockfd, data_blob.c_str() + sent_bytes, data_blob.length() - sent_bytes, 0);
+    sent_bytes += len;
+    if (sent_bytes == data_blob.length()) {
+      close(s->sockfd);
+      // fail to load
+      s->type = State::kClosed;
+    }
+  }
+  inline void CleanClosedStates(void) {
+    size_t top = 0;
+    for (size_t j = 0; j < states.size(); ++j) {
+      if (states[j]->type == State::kClosed) {
+        delete states[j];
+      } else {
+        states[top++] = states[j];        
+      }
+    }
+    states.resize(top);
+  }
+  inline void HandleListen(void) {
+    State *s = new State();
+    s->sockfd = accept(sock_listen, NULL, NULL);
+    s->type = State::kGetName;
+    states.push_back(s);
+  }  
   // create a list of fds we want to poll on 
   inline void CreatePoll(std::vector<pollfd> &fds) {
     fds.clear();
@@ -116,7 +174,6 @@ class Server {
       fread(&s->data_blob[0], s->data_blob.length(), 1, fi);
     }
     // succesfully load file
-    s->type = State::kSendBack;
     write(s->pipefd[1], &sig, sizeof(sig));
   }
   // receive buffer
