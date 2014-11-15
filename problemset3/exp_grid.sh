6247fd20612a0442ac365f18fdb6026e75cad15a
root <root@ip-172-31-42-191.us-west-2.compute.internal>
2014-11-15 01:40:11 +0000

    ok

--- problemset3/exp_grid.sh
+++ problemset3/exp_grid.sh
@@ -1,13 +1,67 @@
 #!/bin/bash
-rm lcagrid.log
+
+rm lcascipy.log
+SN=800
 for ncore in 100 200 400 600
 do
-    for SN in 100 200 400 800 1600
-    do
-	hadoop dfs -rmr data/result-$SN	
-	echo "------------------------" >> lcagrid.log
-	echo "start testing ncore=" $ncore "N=" $SN >>lcagrid.log
-	spark-submit  --total-executor-cores $ncore lcagrid.py data $ncore data/result-$SN 36 1>>lcagrid.log 2>>stderr.log
-	echo "------------------------" >> lcagrid.log
-    done
+    hadoop dfs -rmr data/result-$SN 
+    echo "------------------------" >> lcascipy.log
+    echo "start testing ncore=" $ncore "N=" $SN >>lcascipy.log
+    spark-submit  --total-executor-cores 600 lcascipy.py data $SN data/result-$SN 1>>lcascipy.log
+    echo "------------------------" >> lcascipy.log    
 done
+
+nscore=600
+for SN in 100 200 400 800 1600
+do
+    hadoop dfs -rmr data/result-$SN	
+    echo "------------------------" >> lcascipy.log
+    echo "start testing ncore=" $ncore "N=" $SN >>lcascipy.log
+    spark-submit  --total-executor-cores 600 lcascipy.py data $SN data/result-$SN 1>>lcascipy.log
+    echo "------------------------" >> lcascipy.log
+done
+
+
+rm lcagrid.log
+
+SN=800
+for ncore in 100 200 400 600
+do
+    hadoop dfs -rmr data/gresult-$SN 
+    echo "------------------------" >> lcagrid.log
+    echo "start testing ncore=" $ncore "N=" $SN >>lcagrid.log
+    spark-submit  --total-executor-cores 600 lcagrid.py data $SN 36 data/gresult-$SN 1>>lcagrid.log
+    echo "------------------------" >> lcagrid.log    
+done
+
+nscore=600
+for SN in 100 200 400 800 1600
+do
+    hadoop dfs -rmr data/gresult-$SN	
+    echo "------------------------" >> lcagrid.log
+    echo "start testing ncore=" $ncore "N=" $SN >>lcagrid.log
+    spark-submit  --total-executor-cores 600 lcagrid.py data $SN 36 data/gresult-$SN 1>>lcagrid.log
+    echo "------------------------" >> lcagrid.log
+done
+
+
+for SN in 1600 3200
+do
+    hadoop dfs -rmr data/result-$SN	
+    echo "------------------------" >> lcalarge.log
+    echo "start testing LCA scipy ncore=" $ncore "N=" $SN >>lcalarge.log
+    spark-submit  --total-executor-cores 600 lcascipy.py data $SN data/result-$SN 1>>lcalarge.log
+    echo "------------------------" >> lcalarge.log
+done
+
+for SN in 1600 3200
+do
+    hadoop dfs -rmr data/result-$SN	
+    echo "------------------------" >> lcalarge.log
+    echo "start testing LCA grid ncore=" $ncore "N=" $SN >>lcalarge.log
+    spark-submit  --total-executor-cores 600 lcagrid.py data $SN 36 data/result-$SN 1>>lcalarge.log
+    echo "------------------------" >> lcalarg.log
+done
+
+
+
